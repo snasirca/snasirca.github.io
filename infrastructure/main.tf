06d7b1982025ba96d9871c4a86e7c6befde52d47
Shahriyar Nasir <contact@snasir.ca>
2019-08-12 00:17:04 -0400

    Manage the alias record

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -104,3 +104,15 @@ resource "aws_cloudfront_distribution" "distribution" {
     ssl_support_method = "sni-only"
   }
 }
+
+resource "aws_route53_record" "record" {
+  zone_id = data.aws_route53_zone.zone.id
+  name = var.domain_name
+  type = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.distribution.domain_name
+    zone_id = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
